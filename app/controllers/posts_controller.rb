a9d4e5ad4c3e9f97e97e9d17f3ad57a2c607598f
priom <priom.galaxy@gmail.com>
2016-12-05 17:44:11 -0400

    crud functions added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all.order('created_at DESC')
   end
@@ -15,15 +17,33 @@ class PostsController < ApplicationController
     else
       render 'new'
     end
+  end
 
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
 
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to post_path
   end
 
 
   private
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
   def post_params
     params.require(:post).permit(:title, :body)
   end
