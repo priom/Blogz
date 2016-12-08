8a886ec202ccc7ac489b80d97c69454d17c99ff7
priom <priom.galaxy@gmail.com>
2016-12-07 20:04:42 -0400

    styling merged to master

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # devise_for :users
+
+  devise_for :users, :path => '', :path_names => {:sign_in => 'signin', :sign_out => 'signout', :sign_up => 'signup', :edit => 'edit'}
+
   resources :posts do
     resources :comments
   end
